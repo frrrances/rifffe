9a8ab8f43d28d0c3679b7d03015b62c5e9ffcb3d
Frances Botsford <frances@switchingprotocols.com>
2018-06-26 11:34:17 -0400

    functioning gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,11 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssnano = require('cssnano'),
     del = require('del'),
-    cache = require('gulp-cache'),
-    concat = require('gulp-concat'),
-    imagemin = require('gulp-imagemin'),
     newer = require('gulp-newer'),
     notify = require('gulp-notify'),
     postcss = require('gulp-postcss'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
-    uglify = require('gulp-uglify'),
-    jshint = require('jshint'),
 
     folder = {
       src: 'src/',
@@ -22,18 +17,8 @@ var gulp = require('gulp'),
     }
 ;
 
-// Images
-gulp.task('images', function(){
-  var out = folder.build + 'i/';
-  return gulp.src(folder.src + 'i/**/*.+(png|jpg|gif|svg)')
-  .pipe(newer(out))
-  .pipe(cache(imagemin()))
-  .pipe(gulp.dest(out))
-  .pipe(notify({ message: 'Images task complete' }));
-});
-
 // HTML
-gulp.task('html', ['images'], function() {
+gulp.task('html', function() {
   var
     out = folder.build + 'html/';
   return gulp.src(folder.src + 'html/**/*')
@@ -42,55 +27,36 @@ gulp.task('html', ['images'], function() {
     .pipe(notify({ message: 'HTML task complete' }));
 });
 
-// Scripts
-gulp.task('scripts', function() {
-  var
-    out = folder.build + 'js/';
-  return gulp.src(folder.src + 'js/**/*.js')
-    .pipe(newer(out))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest(out))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest(out))
-    .pipe(notify({ message: 'Scripts task complete' }));
-});
-
 gulp.task('styles', function() {
   var
     out = folder.build + 'css/',
-    opts = [
-      autoprefixer({ browsers: ['last 2 versions'] })
-      ];
+    plugins = [
+        autoprefixer({ browsers: ['last 2 versions'] }),
+        cssnano()
+    ];
   return gulp.src(folder.src + 'scss/main.scss', { style: 'expanded' })
-    .pipe(postcss(opts))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(cssnano())
+    .pipe(sass().on('error', sass.logError))
+    .pipe( postcss([ require('autoprefixer') ]) )
+    .pipe(gulp.dest(out))
+    .pipe( postcss([ require('cssnano') ]) )
+    .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest(out))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
 // clean up time!
 gulp.task('clean', function() {
-    return del(['build/css/*', 'build/html/*', 'build/i/*', 'build/js/*']);
+    return del(['build/css/*']);
 });
 
 gulp.task('default', function() {
-    gulp.start('images', 'html', 'scripts', 'styles');
+    gulp.start('html','styles');
 });
 
 gulp.task('watch', function() {
 
   // Watch .html files
   gulp.watch('src/html/**/*', ['html']);
-
-  // Watch image files
-  gulp.watch('src/i/**/*', ['images']);
-
-  // Watch .js files
-  gulp.watch('src/js/**/*.js', ['scripts']);
 
   // Watch .scss files
   gulp.watch('src/scss/**/*.scss', ['styles']);
